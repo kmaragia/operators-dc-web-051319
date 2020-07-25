91c573ed7469ff38177d3605ee83de12d09abd4c
Kemunto Maragia <kmaragia@email.wm.edu>
2020-07-25 15:39:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,7 @@
 def unsafe? (speed)
-  return speed > 60 || speed < 40 ? true :false
+  return speed > 60 || speed < 40 ? true:false
+end
+
+def not_safe (speed)
+  return speed > 60 || speed < 40 ? true : false
 end
