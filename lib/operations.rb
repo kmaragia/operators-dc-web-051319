3ebf80eb02dc72c4e9ed1cf0aa88151d210be282
Kemunto Maragia <kmaragia@email.wm.edu>
2020-07-25 15:37:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,21 +1,3 @@
-def unsafe?(speed)
-  if speed > 60
-    return "true"
-  end
-  if speed < 40
-    return "true"
-  end
-  if speed < 60 and speed > 40
-    return "false"
-  end
-
+def unsafe? (speed)
+  return speed > 60 || speed < 40 ? true :false
 end
-
-def not_safe?(speed)
-  speed > 60? "true":"false"
-  speed < 40? "true":"false"
-  speed < 60? "false":"true"
-  speed > 40? "false":"true"
-  
-end
-
